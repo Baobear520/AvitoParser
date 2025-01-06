07594d16a64443faba1bc698f081add7befd0778
Dima <aldmikon@gmail.com>
2025-01-06 18:21:01 +0800

    Fixing bugs/minor changes

--- exceptions.py
+++ exceptions.py
@@ -1,2 +1,12 @@
-class MaxRetryAttempsReached(Exception):
+class MaxRetryAttemptsReachedException(Exception):
     """ A custom exception class for exceeding maximum number of retry attempts"""
+    
+    def __init__(self, message="Maximum retries reached"):
+        self.message = message
+        super().__init__(f"{message}")
+class AccessDeniedException(Exception):
+    """A custom exception class in case of IP restrictions """
+    
+    def __init__(self, message="Access denied"):
+        self.message = message
+        super().__init__(f"{message}")
