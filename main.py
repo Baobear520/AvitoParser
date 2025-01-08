6b24dd06d620d37abcc3eab6cc96406716fdc9be
Dima <aldmikon@gmail.com>
2025-01-08 17:51:33 +0800

    Bug fix with save_to_csv argument type, adding saving data to a Postgres db

--- main.py
+++ main.py
@@ -2,20 +2,24 @@ import os
 from browsers import UndetectedChromeBrowser
 from parsers import Parser
 from utilities import runtime_counter
-
+from db import save_to_postgres
 
 @runtime_counter
 def main():
         browser = UndetectedChromeBrowser()
         parser = Parser(browser.get_driver)
         
-        urls = parser.get_urls(category_id=4,total_goal=5000,limit=300,offset=0)
+        urls = parser.get_urls(category_id=4,total_goal=1000,limit=300,offset=0)
         data = parser.scrape(urls)
         
+        os.makedirs('data', exist_ok=True)
         parser.save_to_csv(
             data=data,
-            path=os.makedirs('data', exist_ok=True),
-            filename='output.csv')
+            path='data',
+            filename='output2.csv')
+
+        save_to_postgres(data)
+
 
 if __name__ == "__main__":
     try:
