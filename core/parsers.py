59929abb8cea97a17335829907e26951fc5f9725
Dima <aldmikon@gmail.com>
2025-01-14 13:23:51 +0800

    Refactoring the Parser class (deleting threading options and unused methods, implementing zero-fetch logic and timely script termination when the total_goal is reached)

--- core/parsers.py
+++ core/parsers.py
@@ -1,8 +1,6 @@
-import threading
 import time
 import json
 import random
-from concurrent.futures import ThreadPoolExecutor, as_completed
 
 from selenium.webdriver.remote.webdriver import WebDriver
 from selenium.common.exceptions import TimeoutException
@@ -29,7 +27,6 @@ class Parser:
         self.max_workers = max_workers
         self.base_url = base_url
         self.delay_range = delay_range
-        self.lock = threading.Lock()
 
     def _get_json(self, driver: WebDriver, url: str, delay: int, max_attempts: int = 3) -> dict:
         """Fetch JSON data from a URL using Selenium."""
@@ -61,18 +58,20 @@ class Parser:
         Parse an individual item into a structured object.
 
         :param item: Raw item data from the API response.
-        :param category_name: The category being scraped (e.g., 'real_estate', 'vehicles', 'electronics').
+        :param category_name: The category being scraped (e.g., 'real_estate', 'vehicles', 'electronics','household_equipment').
         :return: Parsed object with the 'object_category' field included.
         """
         obj = {
             'id': item.get('id'),
-            'object_category': category_name,  # Add the category being scraped
-            'subcategory': item.get('category', {}).get('slug'),
+            'category': category_name,
+            'type': item.get('category', {}).get('slug'),
             'title': item.get('title', 'N/A'),
             'price': item.get('priceDetailed', {}).get('string', 'N/A'),
             'price_for': item.get('priceDetailed', {}).get('postfix', ''),
             'location': item.get('location', {}).get('name', 'N/A'),
-            'photo_URLs': [img.get('864x864', '640x640') for img in item.get('images', [])],
+            'photo_URLs': [
+                img.get('864x864', img.get('640x640', None)) for img in item.get('images', [])
+            ],
             'source_URL': item.get('urlPath', '')
         }
 
@@ -87,7 +86,7 @@ class Parser:
         Parse a list of raw data items into structured objects with 'object_category'.
 
         :param data: JSON data containing raw items from the API.
-        :param category: The category being scraped (e.g., 'real_estate', 'vehicles', 'electronics').
+        :param category_name: The category being scraped (e.g., 'real_estate', 'vehicles', 'electronics','household_equipment').
         :return: List of parsed objects.
         """
         objects = []
@@ -98,39 +97,7 @@ class Parser:
             except Exception as e:
                 print(f"Error parsing item: {e}")
         return objects
-    
-    def get_urls(self,
-                category: CategoryType,
-                total_goal: int,
-                limit: int,
-                offset: int,
-                base_url: str,
-                location: bool = False
-                
-    ) -> list:
-        """
-        Generate URLs for scraping.
 
-        :param category: Category enum instance.
-        :param total_goal: Total number of items to fetch.
-        :param limit: Number of items per request.
-        :param offset: Starting offset for the requests.
-        :param base_url: Base URL for the API.
-        :param location: Location filter for the request.
-        :return: List of URLs to scrape.
-        """
-            
-        last_stamp = get_utc_timestamp()
-        category_id = category.category_id  # Get numeric ID from the enum
-
-        urls = []
-        while offset < total_goal * 2:
-            full_url = f'{base_url}forceLocation={location}&lastStamp={last_stamp}&limit={limit}&offset={offset}&categoryId={category_id}'
-            urls.append(full_url)
-           
-            offset += limit * 2
-        print(f"{len(urls)} URLs have been added to the list.")
-        return urls
 
     def url_generator(self, category_id, limit, offset, last_stamp, location):
         """
@@ -145,25 +112,34 @@ class Parser:
         """
         return f"{self.base_url}?forceLocation={location}&lastStamp={last_stamp}&limit={limit}&offset={offset}&categoryId={category_id}"
 
+
     def _worker(self, driver: WebDriver, url: str, category_name: CategoryType, delay: int):
         """Worker function for fetching and parsing data."""
         try:
             json_data = self._get_json(driver, url, delay)
             parsed_data = self._parse_data(json_data, category_name)
-            # with self.lock:
-            #     output.extend(parsed_data)
-            #     print(f"Added {len(parsed_data)} objects.")
             return parsed_data
 
-        except MaxRetryAttemptsReachedException as e:
+        except MaxRetryAttemptsReachedException:
             print(f"Max retries reached for {url}. Moving to the next URL.")
+            return []
         except AccessDeniedException as e:
             print(f"Access denied for {url}. Stopping the script.")
             raise e  # Re-raise the exception to stop the scraping process
         except Exception as e:
             print(f"Unexpected error in worker for {url}: {e}")
+            return []
 
-    def fetch_objects(self, driver, total_goal, limit, location=False):
+
+    def fetch_category_objects(self, driver, category, limit, offset, last_stamp, location):
+        """Fetch objects for a specific category."""
+
+        url = self.url_generator(category.category_id, limit, offset, last_stamp, location)
+        print(f"Fetching data from: {url} for category: {category.verbose_name}")
+        return self._worker(driver, url, category.verbose_name, random.randint(*self.delay_range))
+
+
+    def fetch_objects(self, driver, total_goal, limit, location=False, max_scraping_failures=3):
         """
         Fetch objects dynamically until total_goal is met.
         :parameters:
@@ -172,62 +148,50 @@ class Parser:
             - limit: Number of objects per API call
             - delay: Delay between API requests
             - location: Location filter for the request
+            - max_scraping_failures: Maximum number of consecutive zero-fetch attempts
         """
 
         fetched_objects = []
         offset = 0
         last_stamp = get_utc_timestamp()
 
+        # Initialize the zero-fetch counter to prevent infinite loops
+        scraping_failures_count = 0
+
         while len(fetched_objects) < total_goal:
             try:
                 for category in CategoryType:
-                    # Generate the next URL dynamically
-                    url = self.url_generator(category.category_id, limit, offset, last_stamp, location)
-                    print(f"Fetching data from: {url}")
+                    new_objects = self.fetch_category_objects(driver, category, limit, offset, last_stamp, location)
+                    if new_objects:
+                        scraping_failures_count = 0
+                        fetched_objects.extend(new_objects)
+                        print(f"Added {len(new_objects)} objects. Total: {len(fetched_objects)}/{total_goal}.")
+                    else:
+                        scraping_failures_count += 1
+                        print(
+                            f"No objects fetched for category: {category.verbose_name}."
+                            f"\nZero-fetch count: {scraping_failures_count}/{max_scraping_failures}.")
 
-                    # Fetch and parse data from the current URL
-                    new_objects = self._worker(driver,url,category.verbose_name,random.randint(*self.delay_range))
-                    fetched_objects.extend(new_objects)
-                    print(f"Added {len(new_objects)} objects.")
+                    if len(fetched_objects) >= total_goal:
+                        print(f"Goal reached: {len(fetched_objects)} objects fetched.")
+                        return return_unique_records(fetched_objects)
+
+                    if scraping_failures_count >= max_scraping_failures:
+                        print(
+                            f"Too many consecutive zero-fetch attempts ({scraping_failures_count}). Stopping script.")
+                        return return_unique_records(fetched_objects)
+
                 offset += limit * 2
+
             except AccessDeniedException:
                 print("Access Denied Exception raised. Stopping script.")
-                break  # Stop the loop when access is denied
+                break
 
         return return_unique_records(fetched_objects)
 
-    def _run_thread_pool(self, category: int, urls: list, output: list):
-        """Helper function to run the workers in a thread pool."""
-        with ThreadPoolExecutor(max_workers=5) as executor:
-            driver = self.browser()
-            delay = random.randint(*self.delay_range)
-            futures = [executor.submit(
-                self._worker, driver, url, output, category,delay) for url in urls]
-            
-            for future in as_completed(futures):
-                try:
-                    future.result()  # Wait for the result to be available
-                except AccessDeniedException:
-                    print("Access Denied Exception raised in thread. Stopping script.")
-                    break  # Stop further execution in the thread pool if access is denied
 
 
-    def scrape(self, category: int, urls: list, multithreaded: bool = False) -> list:
-        """Main scraping function."""
-        
-        output = []
-        if multithreaded:
-            self._run_thread_pool(category, urls, output)
-        else:
-            driver = self.browser
-            for url in urls:
-                try:
-                    self._worker(driver, url, output, category, random.randint(*self.delay_range))
-                except AccessDeniedException:
-                    print("Access Denied Exception raised. Stopping script.")
-                    break  # Stop the loop when access is denied
-        print(f"Total: {len(output)} objects.")
-        return return_unique_records(output)
+
 
 
 
