ddcc1fc8eb14a77ebcc132f6ce38ed3b310954cc
Dima <aldmikon@gmail.com>
2025-01-18 15:59:08 +0800

    Renaming Parser -> BaseParser

--- main.py
+++ main.py
@@ -4,8 +4,7 @@ import pandas as pd
 import psycopg2
 
 from core.browsers import UndetectedChromeBrowser
-from core.enums import CategoryType
-from core.parsers import Parser
+from core.parsers import BaseParser
 from core.utilities import runtime_counter, PandasHelper
 from core.db import PostgresDB, DB_HOST, DB_USER, DB_PORT, DB_PASSWORD, DB_NAME, DB_TABLE_NAME, PRODUCT_SCHEMA
 
@@ -14,22 +13,20 @@ from core.db import PostgresDB, DB_HOST, DB_USER, DB_PORT, DB_PASSWORD, DB_NAME,
 def main():
     # Initialize the database
     try:
-        db = PostgresDB(DB_HOST, DB_USER, DB_PORT, DB_PASSWORD, DB_NAME)
-        db.create_table(DB_TABLE_NAME,PRODUCT_SCHEMA)
+        # db = PostgresDB(DB_HOST, DB_USER, DB_PORT, DB_PASSWORD, DB_NAME)
+        # db.create_table(DB_TABLE_NAME,PRODUCT_SCHEMA)
 
         browser = UndetectedChromeBrowser()
-        parser = Parser(browser.get_driver)
+        parser = BaseParser(browser, base_url="https://www.avito.ru/web/1/main/items")
+        data = parser.run(driver=browser.get_driver(), total_goal=1200, limit=300)
 
-        category = CategoryType.ELECTRONICS
-        urls = parser.get_urls(category=category,total_goal=300,limit=300,offset=0)
-        data = parser.scrape(urls)
 
-        db.save_to_db(DB_TABLE_NAME, data)
+        #db.save_to_db(DB_TABLE_NAME, data)
 
         df = pd.DataFrame(data)
         helper = PandasHelper()
         helper.save_data_to_csv_file(
-            df, f'{category.verbose_name}.csv')
+            df, 'experiment1.csv',create_new_file=False)
 
 
     except psycopg2.OperationalError as e:
