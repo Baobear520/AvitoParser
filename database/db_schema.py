316ac7bca511e681e737ed9b024b6aaa45e8179a
Dima <aldmikon@gmail.com>
2025-02-23 16:46:04 +0800

    Refactoring the database schema (correctly assigning one-to-many between users and objects)

--- database/db_schema.py
+++ database/db_schema.py
@@ -1,5 +1,23 @@
 
 DB_SCHEMA = {
+    'users': {
+        'table_name': 'users',
+        'columns': {
+            "id": "BIGSERIAL PRIMARY KEY",
+            "username": "VARCHAR(32) UNIQUE",
+            "phone_number": "VARCHAR(32) UNIQUE",
+            "email": "VARCHAR(64)",
+            "first_name": "VARCHAR(255)",
+            "last_name": "VARCHAR(255)",
+            "income_level": "VARCHAR(255) NULL",
+            "address": "VARCHAR(255)",
+            "gender": "CHAR(2)",
+            "last_updated": "TIMESTAMP DEFAULT CURRENT_TIMESTAMP"
+        },
+        'unique_constraints': ['username', 'phone_number'],
+        'foreign_keys': [],
+        'indexes': []
+    },
     'objects': {
         'table_name': 'objects',
         'columns': {
@@ -16,9 +34,11 @@ DB_SCHEMA = {
             'user_id': 'BIGINT'
         },
         'unique_constraints': ['source_URL'],
+        'foreign_keys': [
+            ('user_id', 'users', 'id')
+        ],
         'indexes': [('user_id', 'idx_user_id'),('category', 'idx_category')]
     },
-
     'unique_records': {
         'table_name': 'unique_records',
         'columns': {
@@ -36,25 +56,7 @@ DB_SCHEMA = {
         'unique_constraints': ['source_URL'],
         'foreign_keys': [],
         'indexes': [('category','idx_category')]
-    },
-
-    'users': {
-        'table_name': 'users',
-        'columns': {
-            "id": "BIGSERIAL PRIMARY KEY",
-            "username": "VARCHAR(32) UNIQUE",
-            "phone_number": "VARCHAR(32) UNIQUE",
-            "email": "VARCHAR(64)",
-            "first_name": "VARCHAR(255)",
-            "last_name": "VARCHAR(255)",
-            "income_level": "VARCHAR(255)",
-            "address": "VARCHAR(255)",
-            "gender": "CHAR(2)",
-            "property": "BIGINT",
-            "last_updated": "TIMESTAMP DEFAULT CURRENT_TIMESTAMP"
-        },
-        'unique_constraints': ['username', 'phone_number'],
-        'foreign_keys': [('property', 'objects', 'id')],
-        'indexes': []
     }
 }
+
+
